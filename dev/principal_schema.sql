b963d78b5eab523b943e7cca3fd5a19a386fc5ce
pehuen <roldanpehuen@gmail.com>
2025-06-06 01:21:42 -0300

    registro mejorado en keycloak

---
+++ dev/principal_schema.sql
@@ -0,0 +1,106 @@
+CREATE SCHEMA IF NOT EXISTS urbancheck;
+SET search_path TO urbancheck;
+
+-- ENUMS
+CREATE TYPE role_enum AS ENUM ('Operador de Atención al vecino', 'Ciudadano', 'Responsable de dependencia', 'Miembro de cuadrilla');
+
+CREATE TYPE municipal_dependency_enum AS ENUM (
+  'Arbolado', 'AreasVerdes', 'Barrido', 'ConservacionDeCalles', 'Electrotecnia',
+  'EspaciosPúblicos', 'Microbasurales', 'ObrasPrivadas', 'ObrasPúblicas',
+  'OficinaTecnicaDeObrasSanitarias', 'PolicíaMunicipal', 'Recolección',
+  'RedDeAgua', 'RedDeCloacas', 'Riego', 'SaludAmbiental', 'Tránsito'
+);
+
+CREATE TYPE priority_level_enum AS ENUM ('Muy Alta', 'Alta', 'Media', 'Baja');
+
+-- TABLES
+CREATE TABLE role (
+  id UUID PRIMARY KEY,
+  description role_enum NOT NULL
+);
+
+CREATE TABLE dependency (
+  id UUID PRIMARY KEY,
+  description TEXT NOT NULL,
+  name municipal_dependency_enum NOT NULL
+);
+
+CREATE TABLE user_account (
+  id UUID PRIMARY KEY,
+  dni BIGINT NOT NULL,
+  email TEXT NOT NULL,
+  email_alt TEXT,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  birth_date DATE NOT NULL,
+  postal_code INT NOT NULL,
+  street TEXT NOT NULL,
+  street_number INT NOT NULL,
+  role_id UUID REFERENCES role(id),
+  dependency_id UUID REFERENCES dependency(id)
+);
+
+CREATE TABLE penalty_type (
+  id UUID PRIMARY KEY,
+  code TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE penalty (
+  id UUID PRIMARY KEY,
+  description UUID REFERENCES penalty_type(id),
+  date TIMESTAMP NOT NULL,
+  user_id UUID REFERENCES user_account(id)
+);
+
+CREATE TABLE subscription (
+  id UUID PRIMARY KEY,
+  user_id UUID REFERENCES user_account(id)
+);
+
+CREATE TABLE ticket_status (
+  id UUID PRIMARY KEY,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE priority (
+  id UUID PRIMARY KEY,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE issue (
+  id UUID PRIMARY KEY,
+  description TEXT NOT NULL,
+  code TEXT NOT NULL,
+  enabled BOOLEAN NOT NULL
+);
+
+CREATE TABLE ticket (
+  id UUID PRIMARY KEY,
+  description VARCHAR(250),
+  latitude DOUBLE PRECISION,
+  longitude DOUBLE PRECISION,
+  timestamp TIMESTAMP NOT NULL,
+  created_by UUID REFERENCES user_account(id),
+  modified_by UUID REFERENCES user_account(id),
+  image_url TEXT,
+  status_id UUID REFERENCES ticket_status(id),
+  priority_id UUID REFERENCES priority(id),
+  issue_id UUID REFERENCES issue(id)
+);
+
+CREATE TABLE status_history (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  date TIMESTAMP NOT NULL,
+  author_id UUID REFERENCES user_account(id),
+  ticket_id UUID REFERENCES ticket(id),
+  status_id UUID REFERENCES ticket_status(id)
+);
+
+CREATE TABLE priority_history (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  date TIMESTAMP NOT NULL,
+  author_id UUID REFERENCES user_account(id),
+  ticket_id UUID REFERENCES ticket(id),
+  priority_id UUID REFERENCES priority(id)
+);
---
+++ dev/urbancheck-theme/login/register.ftl
@@ -0,0 +1,59 @@
+<#import "template.ftl" as layout>
+<@layout.registrationLayout displayInfo=true displayMessage=true; section>
+  <#if section = "title">
+    ${msg("registerTitle")}
+  <#elseif section = "form">
+    <form id="kc-register-form" action="${url.registrationAction}" method="post">
+
+      <div class="form-group">
+        <label for="firstName" class="control-label">${msg("firstName")}</label>
+        <input type="text" id="firstName" name="firstName" value="${(register.formData.firstName!'')}" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="lastName" class="control-label">${msg("lastName")}</label>
+        <input type="text" id="lastName" name="lastName" value="${(register.formData.lastName!'')}" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="email" class="control-label">${msg("email")}</label>
+        <input type="text" id="email" name="email" value="${(register.formData.email!'')}" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="password" class="control-label">${msg("password")}</label>
+        <input type="password" id="password" name="password" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="password-confirm" class="control-label">${msg("passwordConfirm")}</label>
+        <input type="password" id="password-confirm" name="password-confirm" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="user.attributes.dni" class="control-label">DNI</label>
+        <input type="text" id="user.attributes.dni" name="user.attributes.dni" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="user.attributes.postalCode" class="control-label">Código Postal</label>
+        <input type="text" id="user.attributes.postalCode" name="user.attributes.postalCode" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="user.attributes.street" class="control-label">Calle</label>
+        <input type="text" id="user.attributes.street" name="user.attributes.street" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <label for="user.attributes.streetNumber" class="control-label">Altura</label>
+        <input type="text" id="user.attributes.streetNumber" name="user.attributes.streetNumber" class="form-control" />
+      </div>
+
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">${msg("doRegister")}</button>
+      </div>
+
+    </form>
+  </#if>
+</@layout.registrationLayout>
